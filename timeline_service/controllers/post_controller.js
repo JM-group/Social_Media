28b50d95958478c32744698b8cbc09eee3d5fa76
Admin <admin@Jays-MacBook-Air.local>
2019-10-07 14:37:10 -0400

    post add delete edit update done

--- timeline_service/controllers/post_controller.js
+++ timeline_service/controllers/post_controller.js
@@ -24,3 +24,64 @@ exports.create = async(req, res) => {
             res.status(400).send(error)
     }
 }; 
+
+
+// Update or edit information about user
+exports.update = (req, res) => {
+    console.log('insiide update valuuuuueeeeeeeeeeee');
+    console.log(req.params);
+    console.log(req.query);
+    console.log(req.body);
+    PostModel.findByIdAndUpdate(req.query.id, {$set:{
+        user_id: req.body.user_id,
+        community_id: req.body.community_id,
+        description:req.body.description,
+        location:req.body.location,
+        post_media: [{media_url: "url", tags: [{user_name: "dmfskfsdkmfs", user_id: "12", place_name: "Nagai"}] }],
+    }}, {new: true})
+    .then(data => {
+        if(!data) {
+            return res.status(404).send({
+                message: "Record not found with id " + req.params.id
+            });
+        }
+        res.send(data);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Record not found with id " + req.params.id
+            });                
+        }
+        return res.status(500).send({
+            message: "Error updating record with id " + req.params.festiveId
+        });
+    });
+};
+
+
+//Get Post Object
+exports.get = async(req, res) => {
+    console.log('inside geeetttttttttt valueeeeeeee');
+    console.log(req.query);
+    console.log(req.params);
+    try {
+        const post_data_object = await PostModel.findById(req.params.id);
+        res.status(201).send({ post_data_object })        
+    } catch (error) {
+        res.status(400).send(error)
+    }
+};    
+
+
+//Delete Post Object
+exports.delete = async(req, res) => {
+    console.log('inside geeetttttttttt valueeeeeeee');
+    console.log(req.query);
+    console.log(req.params);
+    try {
+        const post_data_object = await PostModel.findByIdAndDelete(req.params.id);
+        res.status(201).send({ "status": "Successfuly deleted post" })        
+    } catch (error) {
+        res.status(400).send(error)
+    }
+};   
--- timeline_service/routes/timeline_routes.js
+++ timeline_service/routes/timeline_routes.js
@@ -5,9 +5,11 @@ module.exports = (app) => {
     app.post('/post', post.create);
 
     // Update user details
+    app.put('/post', post.update)
 
     // Get user details and followers information
+    app.get('/post/:id', post.get);
 
     // Delete a Note with festiveId
-   
+    app.delete('/post/:id', post.delete);   
 }
